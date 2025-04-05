c1d17e17b52a9f8956732336df694dbae3de31d4
saber <qadimi@sadra.tech>
2025-04-05 14:54:39 +0330

    Decorator Custom dto Headers

---
+++ src/property/pipes/request-header.ts
@@ -0,0 +1,30 @@
+import {
+    ExecutionContext,
+    createParamDecorator,
+    BadRequestException,
+  } from '@nestjs/common';
+  import { plainToInstance } from 'class-transformer';
+  import { validate } from 'class-validator';
+  
+  export const RequestHeader = createParamDecorator(
+    async (targetDto: any, ctx: ExecutionContext) => {
+      const request = ctx.switchToHttp().getRequest();
+      const headers = request.headers;
+  
+      const dto = plainToInstance(targetDto, headers, {
+        excludeExtraneousValues: true,
+      });
+  
+      const errors = await validate(dto);
+      if (errors.length > 0) {
+        // تبدیل لیست ارورها به پیام قابل فهم
+        const messages = errors
+          .map(err => Object.values(err.constraints || {}).join(', '))
+          .join('; ');
+        throw new BadRequestException(`Validation failed: ${messages}`);
+      }
+  
+      return dto;
+    },
+  );
+  
--- src/property/property.controller.ts
+++ src/property/property.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Get, HttpCode, Param, ParseBoolPipe, ParseIntPipe, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Headers, HttpCode, Param, ParseBoolPipe, ParseIntPipe, Patch, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CreatePropertyDto } from './dto/createProperty.dto';
 import { idParamDto } from './dto/idParam.dto';
 import { ParseIdPipe } from './pipes/parseIdPipe';
 import { ZodValidationPipe } from './pipes/zodValidationPipe';
 import { CreatePropertySchema, CreatePropertyZodDto } from './dto/createPropertyZod.dto';
+import { HeadersDto } from './dto/headers.dto';
+import { RequestHeader } from './pipes/request-header';
 
 @Controller('property')
 export class PropertyController {
@@ -43,5 +45,12 @@ export class PropertyController {
         @Body() body: CreatePropertyDto) {
         return body;
     }
+
+    @Post('headers')
+    getHeaders(@RequestHeader(HeadersDto) headers: HeadersDto) {
+        return headers;
+    }
+
+
 
 }
